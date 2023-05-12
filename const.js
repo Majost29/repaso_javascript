ba74da519ff11ac1d2cbd0f12ea375016a30e1e8
Majost29 <87915895+Majost29@users.noreply.github.com>
2023-05-12 00:12:23 -0500

    variableConst

--- const.js
+++ const.js
@@ -1,14 +1,16 @@
-CONSTTTTTTTTTTTTTTTTTT;
+//CONSTTTTTTTTTTTTTTTTTT
 //Variables con Const
 
+//Valores Asignados
 const cliente = "Pedro";
 const precioDescuento = 220;
 
-/*//const no se puede reasignar
-cliente = 20;
-//no puede iniciar sin un valor
+//const no se puede reasignar por ejemplo->
+// cliente = 20;
+
+/*no puede iniciar sin un valor
 const precio;
-console.log(precio);*/
+console.log(precio)*/
 
 console.log(cliente);
 console.log(precioDescuento);
