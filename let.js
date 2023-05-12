fa5dcc4593f96da7c87b805957072c928c4a248e
Majost29 <87915895+Majost29@users.noreply.github.com>
2023-05-12 00:14:09 -0500

    variablesLet

--- let.js
+++ let.js
@@ -1,12 +1,17 @@
-//Variables let y const
+//Variables VAR
 /* las variables var ya no se usan son obsoletas y generan errores*/
 
-let cliente = "Juan";
-let precio_descuento = 220; //camelCase variable sy funciones que tiene mas de una palabra (variables o funciones con mas de una palabra 1 en minuscula
-//2 en mayuscula si son tres palabras la primera en mayuscula)
+//Variables LET
+//Ejm de uso
+
+let cliente = "Majos";
+let precio_descuento = 220;
 
 cliente = 20;
 cliente = true;
 console.log(cliente);
 console.log(precio_descuento);
 //variables con let se pueden reasignar y pueden iniciar sin nngun valor
+
+//camelCase variable sy funciones que tiene mas de una palabra (variables o funciones con mas de una palabra 1 en minuscula
+//2 en mayuscula si son tres palabras la primera en mayuscula)
