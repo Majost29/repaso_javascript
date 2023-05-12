0c752b907cd7eb6b1242351232c2012c4a5a74cb
Majost29 <87915895+Majost29@users.noreply.github.com>
2023-05-11 23:04:30 -0500

    add: variables

---
+++ const.js
@@ -0,0 +1,14 @@
+CONSTTTTTTTTTTTTTTTTTT;
+//Variables con Const
+
+const cliente = "Pedro";
+const precioDescuento = 220;
+
+/*//const no se puede reasignar
+cliente = 20;
+//no puede iniciar sin un valor
+const precio;
+console.log(precio);*/
+
+console.log(cliente);
+console.log(precioDescuento);
---
+++ let.js
@@ -0,0 +1,12 @@
+//Variables let y const
+/* las variables var ya no se usan son obsoletas y generan errores*/
+
+let cliente = "Juan";
+let precio_descuento = 220; //camelCase variable sy funciones que tiene mas de una palabra (variables o funciones con mas de una palabra 1 en minuscula
+//2 en mayuscula si son tres palabras la primera en mayuscula)
+
+cliente = 20;
+cliente = true;
+console.log(cliente);
+console.log(precio_descuento);
+//variables con let se pueden reasignar y pueden iniciar sin nngun valor
